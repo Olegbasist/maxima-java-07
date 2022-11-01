2292b34c559d166f6cec4d83859f8060747614cc
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-01 17:34:58 +0700

    Добавил задание

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,9 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+// TODO: 31.10.2022 Для решения задач использовать репозиторий maxima-java-07
+//
+// TODO: 31.10.2022 Описать базовый интерфейс BaseRepository для реализации паттерна DAO с CRUD операциями и выборкой всех объектов (для любого класса) findAll()
+
 public class App 
 {
     public static void main( String[] args )
