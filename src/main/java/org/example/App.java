f637776aea610670e9069088a1fa17851287e4c1
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-10 16:27:45 +0700

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,53 +16,20 @@ import java.util.UUID;
 //      void delete(I id);
 //      List<T> findAll(); //search(), get.. select
 //
-// TODO: Описать класс SimpleCatRepository для реализации этого интерфейса.
+//  Описать класс SimpleCatRepository для реализации этого интерфейса.
 //
-// TODO: URL базы данных и имя таблицы задайте в конструкторе класса выбранным Вами способом
+//  URL базы данных и имя таблицы задайте в конструкторе класса выбранным Вами способом
 public class App 
 {
     public static final String DB_URL = "jdbc:h2:mem:db";
     public static final String DB_DRIVER = "org.h2.Driver";
 
     public static void main( String[] args ) throws ClassNotFoundException, SQLException, IncorrectCatWeightException {
-        //Пробую БД
-        /*System.out.print("Соединяюсь с БД ...");
-        Class.forName(DB_DRIVER);
-        Connection connection;
-        connection = DriverManager.getConnection(DB_URL);
-        System.out.println(" ОК");
-
-        Statement statement = connection.createStatement();
-        statement.executeUpdate("CREATE TABLE cats (Name VARCHAR(30), Weight INT, Angry BIT)");
-        statement.executeUpdate("INSERT INTO cats(Name, Weight, Angry) VALUES ('Cat', 5, 0)");
 
 
-        //connection.close();
-        System.out.println("Соединение с БД закрыто: " +connection.isClosed());
-        //connection = DriverManager.getConnection(DB_URL);
-
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM cats");
-        while (resultSet.next()){
-            String name = resultSet.getString("Name");
-            int weight = resultSet.getInt("Weight");
-            boolean isAngry = resultSet.getBoolean("Angry");
-            System.out.println(name + weight + isAngry);
-        }
-
-
-        System.out.print("Отключаюсь от БД ...");
-        connection.close();
-        System.out.println(" ОК");*/
-        //Соединение с БД -------------------------------------------------
-
-
-
-        //Генерация UUID --------------------------------------------------
-        //System.out.println(UUID.randomUUID());
-
-        // -----------------------------------------------------------------
         SimpleCatRepository simpleCatRepository = new SimpleCatRepository(DB_URL, DB_DRIVER);
 
+        // -----------------------------------------------------------------
         System.out.println("-------------------------------------------");
         Cat cat = new Cat(12L,"Kott",7,false);
         simpleCatRepository.create(cat);
